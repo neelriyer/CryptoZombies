4e6c5f5611174596d63758776d4acaa60aaf2978
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2021-05-08 16:46:07 +1000

    add html front end

--- zombie_ownership.sol
+++ zombie_ownership.sol
@@ -4,7 +4,6 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 import "./safemath.sol";
 
-/// TODO: Replace this with natspec descriptions
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
   using SafeMath for uint256;
@@ -27,13 +26,13 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
-    _transfer(_from, _to, _tokenId);
-  }
+      require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+      _transfer(_from, _to, _tokenId);
+    }
 
   function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-    zombieApprovals[_tokenId] = _approved;
-    emit Approval(msg.sender, _approved, _tokenId);
-  }
+      zombieApprovals[_tokenId] = _approved;
+      emit Approval(msg.sender, _approved, _tokenId);
+    }
 
 }
